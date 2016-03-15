28eaf9c32e6efd7e28e03817494c9aa13b0eab8f
mongooseek <teeninspirit@gmail.com>
2016-03-15 11:28:18 +0200

    minor ref. with app

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ define([
         var router = new Router();
         //APP.io = socketio.connect();
         //APP.io = socketio.disconnect();
-        APP.io.on('custom_response', function (data) {alert(data);});
+        //APP.io.on('custom_response', function (data) {alert(data);});
         Backbone.history.start({silent: true});
     }
 

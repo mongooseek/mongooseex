728e7a499e76f2f6c19fa07e80ff52e3a6a3e08b
mongooseek <teeninspirit@gmail.com>
2016-03-17 13:58:47 +0200

    friends, the rest - show, hide

--- public/js/views/collections/user.js
+++ public/js/views/collections/user.js
@@ -19,7 +19,10 @@ define([
         },
         events: {
             'click .add-to-friends': 'addToFriends',
-            'click #filter-friends': 'filterFriends'
+            'click .refuse-proposition': 'nullifyProposition',
+            'click .cancel-proposition': 'nullifyProposition',
+            'click .confirm-proposition': 'confirmProposition',
+            'click .remove-friend': 'nullifyProposition'
         },
         filterFriends: function () {
             var self = this;
@@ -35,28 +38,121 @@ define([
                 var userView = new UserView({model: friendModel});
             });
         },
+        confirmProposition: function (e) {
+            e.preventDefault();
+            console.log('Clicked confirm');
+            var modelForFriendArray;
+            var modelForUsrArray;
+            var usrModelInFriendArr;
+            var friendModelInUsrArr;
+            var usrIndexInFriendArr;
+            var friendIndexInUsrArr;
+            var friendId;
+            var usrId;
+            var friendModel;
+            var usrModel;
+            var usrFriends;
+            var friendFriends;
+            var added = moment();
+            friendId = e.target.type;
+            usrId = APP.usrId;
+            friendModel = this.collection.get(friendId);
+            usrModel = this.collection.get(usrId);
+            friendFriends = friendModel.get('friends');
+            usrFriends = usrModel.get('friends');
+
+            modelForFriendArray = {'_id': usrId, added: added, status: 'accepted'};
+            modelForUsrArray = {'_id': friendId, added: added, status: 'accepted'};
+
+            friendModelInUsrArr = _.filter(usrFriends, function (friend) {
+                return friend._id == friendId;
+            });
+
+            console.log('friendModelInUsrArr', friendModelInUsrArr);
+            console.log('usrFriends', usrFriends);
+            friendIndexInUsrArr = usrFriends.indexOf(friendModelInUsrArr[0]);
+            console.log('friendIndexInUsrArr', friendIndexInUsrArr);
+
+            usrFriends[friendIndexInUsrArr] = modelForUsrArray;
+
+            usrModelInFriendArr = _.filter(friendFriends, function (friend) {
+                return friend._id == usrId;
+            });
+
+            console.log('usrModelInFriendArr', usrModelInFriendArr);
+            console.log('friendFriends', friendFriends);
+            usrIndexInFriendArr = friendFriends.indexOf(usrModelInFriendArr[0]);
+            console.log('usrIndexInFriendArr', usrIndexInFriendArr);
+
+            friendFriends[usrIndexInFriendArr] = modelForFriendArray;
+
+            friendModel.set({friends: friendFriends, dateOfBirth: moment(usrModel.get('dateOfBirth'))});
+            usrModel.set({friends: usrFriends, dateOfBirth: moment(usrModel.get('dateOfBirth'))});
+
+            friendModel.save({patch: true});
+            usrModel.save({patch: true});
+        },
+        nullifyProposition: function (e) {
+            e.preventDefault();
+            console.log('Clicked add to friends');
+            var usrModelInFriendArr;
+            var friendModelInUsrArr;
+            var usrIndexInFriendArr;
+            var friendIndexInUsrArr;
+            var friendId;
+            var usrId
+            var friendModel;
+            var usrModel;
+            var usrFriends;
+            var friendFriends;
+            friendId = e.target.type;
+            usrId = APP.usrId;
+            friendModel = this.collection.get(friendId);
+            usrModel = this.collection.get(usrId);
+            friendFriends = friendModel.get('friends');
+            usrFriends = usrModel.get('friends');
+
+            usrModelInFriendArr = _.filter(friendFriends, function (friend) {
+                return friend._id == usrId;
+            });
+
+            usrIndexInFriendArr = friendFriends.indexOf(usrModelInFriendArr[0]);
+            friendFriends.splice(usrIndexInFriendArr, 1);
+
+            friendModelInUsrArr = _.filter(friendFriends, function (friend) {
+                return friend._id == friendId;
+            });
+
+            friendIndexInUsrArr = usrFriends.indexOf(friendModelInUsrArr[0]);
+            usrFriends.splice(friendIndexInUsrArr, 1);
+
+            friendModel.set({friends: friendFriends, dateOfBirth: moment(usrModel.get('dateOfBirth'))});
+            usrModel.set({friends: usrFriends, dateOfBirth: moment(usrModel.get('dateOfBirth'))});
+
+            friendModel.save({patch: true});
+            usrModel.save({patch: true});
+        },
         addToFriends: function (e) {
+            e.preventDefault();
             console.log('Clicked add to friends');
             var friendId;
+            var usrId;
             var friendModel;
             var usrModel;
             var usrFriends;
             var friendFriends;
             var modelForFriendArray;
             var modelForUsrArray;
-            var $addToFriends = $('.add-to-friends');
-            var $cancelFriendShip = $('.cancel-friendship');
-            var $readPosts = $('.read-posts');
             var added = moment();
-            e.preventDefault();
             friendId = e.target.type;
+            usrId = APP.usrId;
             friendModel = this.collection.get(friendId);
-            usrModel = this.collection.get(APP.usrId);
+            usrModel = this.collection.get(usrId);
             friendFriends = friendModel.get('friends');
             usrFriends = usrModel.get('friends');
 
-            modelForFriendArray = {'_id': APP.usrId, added: added, status: 'requested'};
-            modelForUsrArray = {'_id': friendId, added: added, status: 'pending'};
+            modelForFriendArray = {'_id': usrId, added: added, status: 'pending'};
+            modelForUsrArray = {'_id': friendId, added: added, status: 'requested'};
 
             friendFriends.push(modelForFriendArray);
             usrFriends.push(modelForUsrArray);
@@ -66,17 +162,6 @@ define([
 
             friendModel.save({patch: true});
             usrModel.save({patch: true});
-
-            $addToFriends.hide();
-            $cancelFriendShip.show();
-            $readPosts.show();
-
-
-            /*if (usrFriends.indexOf(friendId) == -1 && friendId != APP.usrId) {
-                usrFriends.push(friendId);
-                usrModel.set({friends: usrFriends, dateOfBirth: moment(usrModel.get('dateOfBirth'))});
-                usrModel.save();
-            }*/
         },
         render: function () {
             console.log('Clicked USERS BUTTON');
--- public/js/views/models/user.js
+++ public/js/views/models/user.js
@@ -3,34 +3,39 @@ define([
     'Backbone',
     'Underscore',
     'jQuery',
-    'models/user',
-    'text!templates/models/user.html',
-    'Moment',
-], function (Backbone, _, $, UserModel, userTemplate, moment) {
+    //'models/user',
+    //'text!templates/models/user.html',
+    'Moment'
+], function (Backbone, _, $, moment) {
     console.log("I am inside user view");
     var UserView = Backbone.View.extend({
         el: '.user-item',
-        tmpl: _.template(userTemplate),
+        //tmpl: _.template(userTemplate),
         initialize: function () {
             console.log('User VIEW and User MODEL initialized!!!');
             this.render();
         },
         events: {},
         render: function () {
+            var friendshipTemplateUrl;
             var self = this;
             var friendsArr;
             var my;
             var myStatus;
-            var iAmInFriends;
+            var iAmNotInFriends;
             friendsArr = self.model.get('friends');
             var $templateForUsers = $('#template-for-users');
             if ($templateForUsers.attr('id')) {
                 my = _.filter(friendsArr, function (me) {
                     return (me._id == APP.usrId);
                 });
-                iAmNotInFriends = my[0]==undefined;
-                myStatus = (iAmNotInFriends)?'add':my[0].status;
-                self.$el.append(self.tmpl(self.model.toJSON()));
+                iAmNotInFriends = my[0] == undefined;
+                myStatus = (iAmNotInFriends) ? 'add' : my[0].status;
+                friendshipTemplateUrl = 'text!templates/models/friendshipStatus/' + myStatus + '.html';
+                require([friendshipTemplateUrl], function (template) {
+                    self.tmpl = _.template(template);
+                    self.$el.append(self.tmpl(self.model.toJSON()));
+                });
                 return this;
             }
         }

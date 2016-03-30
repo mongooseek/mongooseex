2000464a0125f182642463537d59ad3a0014b7a7
mongooseek <teeninspirit@gmail.com>
2016-03-30 12:43:25 +0300

    wo with usr fr ...

--- public/js/views/models/friend.js
+++ public/js/views/models/friend.js
@@ -3,11 +3,12 @@ define([
     'Backbone',
     'Underscore',
     'jQuery',
-], function (Backbone, _, $) {
+    'text!templates/models/friend.html',
+], function (Backbone, _, $, template) {
     console.log("I am inside user view");
     var FriendView = Backbone.View.extend({
         el: '.friend-item',
-        //tmpl: _.template(userTemplate),
+        tmpl: _.template(template),
         initialize: function () {
             console.log('FRIEND VIEW was INITIALIZED!!!');
             this.render();
@@ -23,16 +24,17 @@ define([
             friendsArr = self.model.get('friends');
             var $templateForUsers = $('#template-for-friends');
             if ($templateForUsers.attr('id')) {
-                my = _.filter(friendsArr, function (me) {
-                    return (me._id == APP.usrId);
-                });
-                iAmNotInFriends = my[0] == undefined;
-                myStatus = (iAmNotInFriends) ? 'add' : my[0].status;
-                friendshipTemplateUrl = 'text!templates/models/friendshipStatus/' + myStatus + '.html';
-                require([friendshipTemplateUrl], function (template) {
-                    self.tmpl = _.template(template);
-                    self.$el.append(self.tmpl(self.model.toJSON()));
-                });
+                /*my = _.filter(friendsArr, function (me) {
+                 return (me._id == APP.usrId);
+                 });
+                 iAmNotInFriends = my[0] == undefined;
+                 myStatus = (iAmNotInFriends) ? 'add' : my[0].status;
+                 friendshipTemplateUrl = 'text!templates/models/friendshipStatus/' + myStatus + '.html';
+                 require([friendshipTemplateUrl], function (template) {
+                 self.tmpl = _.template(template);
+                 self.$el.append(self.tmpl(self.model.toJSON()));
+                 });*/
+                self.$el.append(self.tmpl(self.model.toJSON()));
                 return this;
             }
         }
